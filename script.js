3b2a0cfa5eceedd04f799f6db1aabe990a78eedd
helioramon <helio.alvareng@gmail.com>
2022-06-15 02:46:29 -0300

    counting odd and even numbers using for and while

---
+++ script.js
@@ -0,0 +1,22 @@
+for (let count = 1; count <= 10; count++){
+    console.log (count);
+}
+
+let count = 10
+
+while(0 ==! count){
+    console.log(count);
+    count --
+}
+
+for (let count = 1; count <= 100; count++){
+    if (count % 2 !== 0){
+    console.log(count)
+    }
+}
+
+for (let count = 0; count<101; count++){
+    if (count % 2 === 0){
+        console.log(count)
+    }
+}
